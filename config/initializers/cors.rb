4b121a8480f09ea7f30cce7598973cd18cc5e82c
lukesherwood <luke.sherwood55@gmail.com>
2021-01-30 10:54:42 +1300

    removed / from  cors origins to netlify  to test heroku app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mowbrayrecipes.netlify.app/'
+    origins 'https://mowbrayrecipes.netlify.app'
 
     resource '*',
       headers: :any,
