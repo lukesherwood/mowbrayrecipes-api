5597d88126fe1a78b374a3763b39dc009e5f1c42
lukesherwood <luke.sherwood55@gmail.com>
2021-02-13 09:43:24 -0500

    changed cors policy to allow dev and prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://mowbrayrecipes.netlify.app'
-
+    resource '*',
+      headers: :any,
+      expose: ["Authorization"],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  
+  allow do
+    origins 'http://localhost:3000'
     resource '*',
       headers: :any,
       expose: ["Authorization"],
