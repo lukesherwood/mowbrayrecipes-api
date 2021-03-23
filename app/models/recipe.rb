d6017898925d682657a46b837ba8fcdf74ac7741
lukesherwood <luke.sherwood55@gmail.com>
2021-03-23 10:09:50 -0400

    change recipe to dependent destroy

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   include Rails.application.routes.url_helpers
   has_one_attached :image
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :comments, allow_destroy: true
 
   def get_image_url
